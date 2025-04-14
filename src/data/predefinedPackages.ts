eb9418c77a85ac97c1376f235e8472fbdbce34b5
Nobeat12 <yayishop.contacte@gmail.com>
2025-04-14 18:56:06 +0100

    Update predefinedPackages.ts

--- src/data/predefinedPackages.ts
+++ src/data/predefinedPackages.ts
@@ -15,7 +15,7 @@ export const predefinedPackages: Package[] = [
     receiptLocation: "Brugg AG",
     receiptDate: "09-04-2025",
     deliveryLocation: "Rue Gardiol 6BIS, 1218 Le Grand-Saconnex, Suisse",
-    status: "En cours de livraison",
+    status: "In Zustellung",
     customerInfo: "IPHONE 15 PRO MAX"
   },
 ];
